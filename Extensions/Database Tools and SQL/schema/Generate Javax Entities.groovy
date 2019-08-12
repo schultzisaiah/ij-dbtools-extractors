205a00de7eee15bb0428421d5bdb1a86c7c101d0
Isaiah Schultz <isaiah.schultz@tr.com>
2019-08-12 14:34:04 -0500

    Adding a potentially helpful comment...

--- Extensions/Database Tools and SQL/schema/Generate Javax Entities.groovy
+++ Extensions/Database Tools and SQL/schema/Generate Javax Entities.groovy
@@ -12,15 +12,15 @@ import com.intellij.database.util.DasUtil
 
 packageName = "com.app.persistence.entity;"
 typeMapping = [
-  (~/(?i)int/)                      : "Long",
-  (~/(?i)float|double|decimal|real/): "Double",
+        (~/(?i)int/)                      : "Long",
+        (~/(?i)float|double|decimal|real/): "Double",
 //  (~/(?i)datetime|timestamp/)       : "java.sql.Timestamp",
 //  (~/(?i)date/)                     : "java.sql.Date",
 //  (~/(?i)time/)                     : "java.sql.Time",
-  (~/(?i)datetime|timestamp/)       : "java.time.LocalDateTime",
-  (~/(?i)date/)                     : "java.time.LocalDateTime",
-  (~/(?i)time/)                     : "java.time.LocalDateTime",
-  (~/(?i)/)                         : "String"
+        (~/(?i)datetime|timestamp/)       : "java.time.LocalDateTime",
+        (~/(?i)date/)                     : "java.time.LocalDateTime",
+        (~/(?i)time/)                     : "java.time.LocalDateTime",
+        (~/(?i)/)                         : "String"
 ]
 
 FILES.chooseDirectoryAndSave("Choose directory", "Choose where to store generated files") { dir ->
@@ -59,6 +59,32 @@ def generate(out, table, className, fields) {
     out.println "  private ${it.type} ${it.name};"
   }
   out.println "}"
+  out.println ""
+  out.println "/* TODO: This assumes that, if you need it, you already have this converter implemented: "
+  out.println "package $packageName\n" +
+          "\n" +
+          "import java.time.LocalDateTime;\n" +
+          "import java.time.ZoneId;\n" +
+          "import java.util.Date;\n" +
+          "import javax.persistence.AttributeConverter;\n" +
+          "import javax.persistence.Converter;\n" +
+          "\n" +
+          "@Converter(autoApply = true)\n" +
+          "public class DateToLocalDateTimeConverter implements AttributeConverter<LocalDateTime, Date> {\n" +
+          "\n" +
+          "  @Override\n" +
+          "  public Date convertToDatabaseColumn(LocalDateTime attribute) {\n" +
+          "    return attribute == null ? null\n" +
+          "        : Date.from(attribute.atZone(ZoneId.systemDefault()).toInstant());\n" +
+          "  }\n" +
+          "\n" +
+          "  @Override\n" +
+          "  public LocalDateTime convertToEntityAttribute(Date dbData) {\n" +
+          "    return dbData == null ? null\n" +
+          "        : LocalDateTime.ofInstant(dbData.toInstant(), ZoneId.systemDefault());\n" +
+          "  }\n" +
+          "}\n" +
+          "*/"
 }
 
 def calcFields(table) {
@@ -67,9 +93,9 @@ def calcFields(table) {
     String annos = getAnnotationString(table, col, spec)
     def typeStr = typeMapping.find { p, t -> p.matcher(spec).find() }.value
     fields += [[
-                 name : javaName(col.getName(), false),
-                 type : typeStr,
-                 annos: annos]]
+                       name : javaName(col.getName(), false),
+                       type : typeStr,
+                       annos: annos]]
   }
 }
 
@@ -97,8 +123,8 @@ def getAnnotationString(table, col, spec) {
 
 def javaName(str, capitalize) {
   def s = com.intellij.psi.codeStyle.NameUtil.splitNameIntoWords(str)
-    .collect { Case.LOWER.apply(it).capitalize() }
-    .join("")
-    .replaceAll(/[^\p{javaJavaIdentifierPart}[_]]/, "_")
+          .collect { Case.LOWER.apply(it).capitalize() }
+          .join("")
+          .replaceAll(/[^\p{javaJavaIdentifierPart}[_]]/, "_")
   capitalize || s.length() == 1? s : Case.LOWER.apply(s[0]) + s[1..-1]
 }
