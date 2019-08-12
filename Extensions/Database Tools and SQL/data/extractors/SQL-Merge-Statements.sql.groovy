cc38107cbdd970fab8126f38e17c67271525f55d
Isaiah Schultz <isaiah.schultz@tr.com>
2019-08-12 15:00:44 -0500

    adding in missing comments

--- Extensions/Database Tools and SQL/data/extractors/SQL-Merge-Statements.sql.groovy
+++ Extensions/Database Tools and SQL/data/extractors/SQL-Merge-Statements.sql.groovy
@@ -1,3 +1,21 @@
+/*
+ * Available context bindings:
+ *   COLUMNS     List<DataColumn>
+ *   ROWS        Iterable<DataRow>
+ *   OUT         { append() }
+ *   FORMATTER   { format(row, col); formatValue(Object, col) }
+ *   TRANSPOSED  Boolean
+ * plus ALL_COLUMNS, TABLE, DIALECT
+ *
+ * where:
+ *   DataRow     { rowNumber(); first(); last(); data(): List<Object>; value(column): Object }
+ *   DataColumn  { columnNumber(), name() }
+ *
+ *
+ * columns[0] is assumed to be the pk field. I'm still sifting through the ij source code to see if
+ * I can get access to any sort of "is pk" on the columns so that this can function more accurately.
+ */
+
 SEP = ", "
 QUOTE     = "\'"
 NEWLINE   = System.getProperty("line.separator")
